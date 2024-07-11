ad53767625ffbef7ae9c5543102b31018e167eee
Kanamaru <128622577+KoheiK01@users.noreply.github.com>
2024-07-11 17:02:40 +0900

    Update paiza_LevelUp_C_データセット選択メニュー

---
+++ paiza_LevelUp_C/データセット選択メニュー.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+class Program
+{
+  static void Main()
+  {
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    /********** 動的配列 **********/
+    //1問目
+    //int[] fLine = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    //int[] A = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    //Console.WriteLine(A[fLine[1] - 1]);
+    //2問目
+    //int N = int.Parse(Console.ReadLine());
+    //int[] A = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    //int Q = int.Parse(Console.ReadLine());
+    //int[] B = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    //for (int i = 0; i < Q; i++)
+    //{
+    //  Console.WriteLine(A[B[i] - 1]);
+    //}
+    //3問目
+    //int[] ABC = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    //Console.WriteLine(ABC.Max() - ABC.Min());
+    //4問目
+    //int[] fLine = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    //List<int> A = Console.ReadLine().Split().Select(int.Parse).ToList();
+    //for (int i = 0; i < fLine[1]; i++)
+    //{
+    //  int[] qi= Console.ReadLine().Split().Select(int.Parse).ToArray();
+    //  if (qi[0] == 0)
+    //  {
+    //    A.Add(qi[1]);
+    //  }
+    //  else if (qi[0] == 1)
+    //  {
+    //    A.RemoveAt(A.Count - 1);
+    //  }
+    //  else
+    //  {
+    //    Console.WriteLine(string.Join(" ",A));
+    //  }
+    //}
+    /********** 文字列の配列 **********/
+    //1～2問目
+    //Console.WriteLine(Console.ReadLine());
+    //3問目
+    //int[] fLine = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    //char[,] _maps = new char[fLine[0], fLine[1]];
+    //for (int i = 0; i < fLine[0]; i++)
+    //{
+    //  string s = Console.ReadLine();
+    //  for (int j = 0; j < s.Length; j++)
+    //  {
+    //    _maps[i, j] = s[j];
+    //  }
+    //}
+    //string ans = "No";
+    //if (_maps[fLine[2] - 1, fLine[3] - 1] == '#')
+    //{
+    //  ans = "Yes";
+    //}
+    //Console.WriteLine(ans);
+    /********** 商品の検索 **********/
+    /********** 集合の結合 **********/
+  }
+}
