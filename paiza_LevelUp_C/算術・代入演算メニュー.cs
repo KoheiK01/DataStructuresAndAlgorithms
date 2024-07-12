435cc9b229863ba4edc086974518368ff34e76b3
Kanamaru <128622577+KoheiK01@users.noreply.github.com>
2024-07-12 14:06:31 +0900

    Finish paiza_LevelUp_C_算術・代入演算メニュー

---
+++ paiza_LevelUp_C/算術・代入演算メニュー.cs
@@ -0,0 +1,47 @@
+﻿//using System;
+//using System.Linq;
+//using System.Runtime.InteropServices;
+
+//class Program
+//{
+//  static void Main()
+//  {
+//    // 自分の得意な言語で
+//    // Let's チャレンジ！！
+//    /********** 累乗 2 **********/
+//    //1問目
+//    //int[] _ints = { 1231, 5178 };
+//    //Console.WriteLine(_ints.Sum(x => x));
+//    //2問目
+//    //int[] _ints = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+//    //Console.WriteLine($"{_ints[0] - _ints[1]} {_ints[0] * _ints[1]}");
+//    //3問目
+//    //int[] _ints = { 437326, 9085 };
+//    //Console.WriteLine($"{_ints[0] / _ints[1]} {_ints[0] % _ints[1]}");
+//    //4問目
+//    //int[] _ints = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+//    //int A = _ints[0], B = _ints[1], C = _ints[2];
+//    //Console.WriteLine($"{A * A} {B * B + C * C}");
+//    //5問目
+//    //int A = 202, B = 134, C = 107;
+//    //Console.WriteLine(Math.Pow((A + B) * C, 2));
+//    //6問目
+//    //int[] _ints = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+//    //int A = _ints[0], B = _ints[1], C = _ints[2], D = _ints[3];
+//    //Console.WriteLine(Math.Pow((A+B)*C,2)%D);
+//    /********** 代入演算 4 **********/
+//    //1～2問目
+//    //int[] _ints = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+//    //int A = _ints[0], B = _ints[1], C = _ints[2];
+//    //int N = 0;
+//    //N = (N + A) * B % C;
+//    //Console.WriteLine(N);
+//    //3～4問目
+//    //int N = 10000;
+//    //int[] _ints = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+//    //int A = _ints[0], B = _ints[1];
+//    ////int A = 361, B = 28;
+//    //N = N / A % B;
+//    //Console.WriteLine(N);
+//  }
+//}
