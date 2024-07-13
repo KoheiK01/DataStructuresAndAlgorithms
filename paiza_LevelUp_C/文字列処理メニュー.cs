3fbd11eb00e3e79ff1fa06e2b9cdea249bf04f4b
Kanamaru <128622577+KoheiK01@users.noreply.github.com>
2024-07-14 02:11:14 +0900

    Update paiza_LevelUp_C_文字列処理メニュー

---
+++ paiza_LevelUp_C/文字列処理メニュー.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+class Program
+{
+  static void Main()
+  {
+    //自分の得意な言語で
+    //Let's チャレンジ！！
+    //1問目
+    //Console.WriteLine("paiza");
+    //2問目
+    //Console.WriteLine(Console.ReadLine());
+    //3問目
+    //string S = Console.ReadLine();
+    //int index = int.Parse(Console.ReadLine()) - 1;
+    //Console.WriteLine(S[index]);
+    //4問目
+    //string S = Console.ReadLine();
+    //Console.WriteLine(S == "paiza" ? "YES" : "NO");
+    //5問目
+    //string S=Console.ReadLine();
+    //Console.WriteLine(S.Length);
+    //6問目
+    //string S = Console.ReadLine();
+    //string T = Console.ReadLine();
+    //Console.WriteLine(S.IndexOf(T) + 1);
+    //7問目
+    //int N = int.Parse(Console.ReadLine());
+    //string ans = "";
+    //for (int i = 0; i < N; i++)
+    //{
+    //  ans += Console.ReadLine();
+    //}
+    //Console.WriteLine(ans);
+    //8問目
+    //string S = Console.ReadLine();
+    //int[] line = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+    //int start = line[0] - 1, count = line[1] - start;
+    //Console.WriteLine(S.Substring(start,count));
+    //9問目
+    //string S = Console.ReadLine();
+    //string T = Console.ReadLine();
+    //int N = int.Parse(Console.ReadLine());
+    //Console.WriteLine(S.Insert(N, T));
+    //10問目
+    //string S = Console.ReadLine();
+    //string[] sLine = Console.ReadLine().Split();
+    //int index = int.Parse(sLine[0]) - 1;
+    //string c = sLine[1];
+    //S = S.Substring(0, index) + c + S.Substring(index + 1);
+    //Console.WriteLine(S);
+    //11問目
+    int N = int.Parse(Console.ReadLine());
+    Console.WriteLine(N - 813);
+  }
+}
