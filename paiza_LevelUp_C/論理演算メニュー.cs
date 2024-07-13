0c098b4730daa258c7c13634e7d9c5df33182eaf
Kanamaru <128622577+KoheiK01@users.noreply.github.com>
2024-07-13 16:54:02 +0900

    Finish paiza_LevelUp_C_論理演算メニュー

---
+++ paiza_LevelUp_C/論理演算メニュー.cs
@@ -0,0 +1,46 @@
+﻿//using System;
+//using System.Linq;
+
+//class Program
+//{
+//  static void Main()
+//  {
+//    //自分の得意な言語で
+//    //Let's チャレンジ！！
+//    /********** 論理演算を用いた計算のまとめ **********/
+//    //1～2・4～7問目
+//    //int[] fline = Console.ReadLine().Split().Select(int.Parse).ToArray();
+//    //int A = fline[0];
+//    //int B = fline[1];
+//    //Console.WriteLine(A & B);
+//    //Console.WriteLine(A | B);
+//    //Console.WriteLine(A ^ B);
+//    //Console.WriteLine((A & B) == 1 ? 0 : 1);
+//    //Console.WriteLine((A | B) == 1 ? 0 : 1);
+//    //Console.WriteLine((A ^ B) == 1 ? 0 : 1);
+//    //Console.WriteLine((A & B) + " " + (A ^ B));
+//    //3問目
+//    //int A = int.Parse(Console.ReadLine());
+//    //Console.WriteLine(A == 0 ? 1 : 0);
+//    ////8問目
+//    //int[] fline = Console.ReadLine().Split().Select(int.Parse).ToArray();
+//    //int A = fline[0];
+//    //int B = fline[1];
+//    //int C1 = fline[2];
+//    //int CX, CY, S;
+//    //HalfAdder(A, B, out CX, out S);
+//    //HalfAdder(C1, S,out CY,out S);
+//    //int C = CX ^ CY;
+//    //Console.WriteLine(C + " " + S);
+//    //9問目
+//    //int[] fline = Console.ReadLine().Split().Select(int.Parse).ToArray();
+//    //int A = fline[0], B = fline[1], C = fline[2], D = fline[3];
+//    //int A_ = A == 0 ? 1 : 0, B_ = B == 0 ? 1 : 0, C_ = C == 0 ? 1 : 0;
+//    //Console.WriteLine((((A_ & B_) | C_) == 0 ? 1 : 0) ^ D);
+//  }
+//  //private static void HalfAdder(int a, int b, out int C, out int S)
+//  //{
+//  //  C = a & b;
+//  //  S = a ^ b;
+//  //}
+//}
