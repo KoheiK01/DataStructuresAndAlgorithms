5105d85cbcf075d8475aa67535aa49544e544fe5
Kanamaru <128622577+KoheiK01@users.noreply.github.com>
2024-07-16 14:40:13 +0900

    修正_Igarashi_データ構造毎にクラス分け

---
+++ igarashi/c3_BinarySearchTree.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+/******************* 二分探索木 *******************/
+/// <summary>
+/// 二分探索木を実装します．
+/// ヒープではその構造を使用して配列で扱えます．
+/// 二分探索木では一般にノードクラスを定義します．
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class Node<T> where T : IComparable<T>
+{
+  public T Data;
+  public Node<T> Left;
+  public Node<T> Right;
+
+  public Node(T _data)
+  {
+    Data = _data;
+    Left = null;
+    Right = null;
+  }
+}
+
+/// <summary>
+/// 探索木の実装クラスです．
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class BinarySearchTree<T> where T : IComparable<T>
+{
+  public Node<T> Root;
+
+  public BinarySearchTree()
+  {
+    Root = null;
+  }
+
+  /// <summary>
+  /// 要素の挿入を行います
+  /// </summary>
+  /// <param name="_data"></param>
+  public void Insert(T _data)
+  {
+    Root = InsertRec(Root, _data);
+  }
+
+  /// <summary>
+  /// 挿入のために再帰的に行います．
+  /// </summary>
+  /// <param name="_node"></param>
+  /// <param name="_data"></param>
+  /// <returns></returns>
+  private Node<T> InsertRec(Node<T> _node, T _data)
+  {
+    //nullならばその場所に要素を挿入する．
+    //そのために，return _nodeを行う．
+    if (_node == null)
+    {
+      _node = new Node<T>(_data);
+      return _node;
+    }
+
+    //挿入ノードの値が比較ノードの値よりも小さいならば，Leftを探索
+    if (_data.CompareTo(_node.Data) < 0)
+    {
+      _node.Left = InsertRec(_node.Left, _data);
+    }
+    //大きいならば，Rightを探索
+    else
+    {
+      _node.Right = InsertRec(_node.Right, _data);
+    }
+    return _node;
+  }
+
+  /// <summary>
+  /// 要素の削除を行う．
+  /// </summary>
+  /// <param name="_data"></param>
+  public void Delete(T _data)
+  {
+    Root = DeleteRec(Root, _data);
+  }
+
+  /// <summary>
+  /// 要素の削除を行うために再帰的に行う．
+  /// まずは削除ノードを発見し，二つの子ノードを持っているならば
+  /// 右木部分の最小値を削除ノードにあてはめる．
+  /// </summary>
+  /// <param name="_node"></param>
+  /// <param name="_data"></param>
+  /// <returns></returns>
+  private Node<T> DeleteRec(Node<T> _node, T _data)
+  {
+    if (_node == null) { return null; }
+
+    //削除ノードの値は比較ノードの値よりも小さいので，左木部分に存在する．
+    if (_data.CompareTo(_node.Data) < 0)
+    {
+      _node.Left = DeleteRec(_node.Left, _data);
+    }
+    //大きいので右木部分に存在する．
+    else if (_data.CompareTo(_node.Data) > 0)
+    {
+      _node.Right = DeleteRec(_node.Right, _data);
+    }
+    //同じならば削除ノードを消してその部分に子ノードを代入する．
+    else
+    {
+      //子ノードが二つならば右木部分の最小値を削除ノードにあてはめる．
+      //あてはめたノードも削除したと考えられるので，削除を行う．
+      if (_node.Left != null && _node.Right != null)
+      {
+        Node<T> _minNode = GetMinNode(_node.Right);
+        _node.Data = _minNode.Data;
+        _minNode.Right = DeleteRec(_node.Right, _minNode.Data);
+      }
+      //子ノードが一つならば子ノードを親ノードにあてはめるのみ．
+      else if (_node.Left != null)
+      {
+        return _node.Left;
+      }
+      else
+      {
+        return _node.Right;
+      }
+    }
+    return _node;
+  }
+
+  /// <summary>
+  /// 右子ノードのうち最小のものをとってくる．
+  /// 最小のノード＝左子ノードがないノード
+  /// </summary>
+  /// <param name="_node"></param>
+  private Node<T> GetMinNode(Node<T> _node)
+  {
+    if (_node.Left != null)
+    {
+      return GetMinNode(_node.Left);
+    }
+    return _node;
+  }
+}
+//public class Example
+//{
+//  public static void Main()
+//  {
+//    BinarySearchTree<int> _tree = new BinarySearchTree<int>();
+//    _tree.Insert(34);
+//    _tree.Insert(51);
+//    _tree.Insert(72);
+//    _tree.Insert(17);
+//    _tree.Insert(66);
+//    _tree.Insert(35);
+//    _tree.Insert(99);
+//    _tree.Insert(1);
+//    _tree.Delete(51);
+//    _tree.Delete(34);
+//    _tree.Delete(1);
+//  }
+//}
---
+++ igarashi/c3_Heap.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+/// <summary>
+/// 最小ヒープを実装しているクラスです．
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class Heap<T> where T : IComparable<T>
+{
+  private List<T> data;
+
+  public Heap()
+  {
+    data = new List<T>();
+  }
+
+  public void Insert(T _item)
+  {
+    data.Add(_item);
+    int _child = data.Count - 1;
+    while (_child > 0)
+    {
+      int _parent = (_child - 1) / 2;
+      if (data[_child].CompareTo(data[_parent]) >= 0)
+      {
+        break;
+      }
+
+      T _temp = data[_child];
+      data[_child] = data[_parent];
+      data[_parent] = _temp;
+
+      _child = _parent;
+    }
+  }
+
+  public int Count { get { return data.Count; } }
+
+  public T DeleteMin()
+  {
+    if (data.Count == 0) throw new InvalidOperationException("The heap is empty");
+
+    T _min = data[0];
+    data[0] = data[data.Count - 1];
+    data.RemoveAt(data.Count - 1);
+
+    int _parent = 0;
+
+    while (2 * _parent + 1 <= data.Count - 1)
+    {
+      int _leftChild = 2 * _parent + 1;//親から見た左子ノード
+      int _rightChild = 2 * _parent + 2;//親から見た右子ノード
+      int _minChild = _leftChild;
+
+      if (_rightChild <= data.Count - 1 && data[_leftChild].CompareTo(data[_rightChild]) > 0)
+      {
+        _minChild = _rightChild;
+      }
+
+      if (data[_minChild].CompareTo(data[_parent]) < 0)
+      {
+        T _temp = data[_minChild];
+        data[_minChild] = data[_parent];
+        data[_parent] = _temp;
+
+        _parent = _minChild;
+      }
+      else break;
+    }
+    return _min;
+  }
+}
+
+//public class Example
+//{
+//  public static void Main()
+//  {
+//    Heap<int> _heap = new Heap<int>();
+//    _heap.Insert(31);
+//    _heap.Insert(21);
+//    _heap.Insert(48);
+//    _heap.Insert(9);
+//    _heap.DeleteMin();
+//    _heap.Insert(26);
+//    _heap.Insert(13);
+//    _heap.DeleteMin();
+
+//    while (_heap.Count > 0)
+//    {
+//      Console.WriteLine(_heap.DeleteMin());
+//    }
+//  }
+//}
