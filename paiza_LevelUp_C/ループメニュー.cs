e2e9ff8cdc6801005533d5a853ef6bf56739cb92
Kanamaru <128622577+KoheiK01@users.noreply.github.com>
2024-07-26 18:22:31 +0900

    Finish paiza_LevelUp_C_ループメニュー1

---
+++ paiza_LevelUp_C/ループメニュー.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Reflection.Emit;
+
+class Program
+{
+  static void Main()
+  {
+    //自分の得意な言語で
+    //Let's チャレンジ！！
+    /********** 0 が含まれていないか判定 **********/
+    //1問目
+    //string S = Console.ReadLine();
+    //string T = "paiza";
+    //string result = S == T ? "YES" : "NO";
+    //Console.WriteLine(result);
+    //2問目
+    //int N = int.Parse(Console.ReadLine());
+    //string result = N <= 100 ? "YES" : "NO";
+    //Console.WriteLine(result);
+    //3問目
+    //int[] ABC = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+    //int A = ABC[0], B = ABC[1], C = ABC[2];
+    //string result = A*B <= C ? "YES" : "NO";
+    //Console.WriteLine(result);
+    //4問目
+    //int N = int.Parse(Console.ReadLine());
+    //string result = N != 0 ? "YES" : "NO";
+    //Console.WriteLine(result);
+    //5問目
+    //int N = int.Parse(Console.ReadLine());
+    //string result = "YES";
+    //for (int i = 0; i < N; i++)
+    //{
+    //  int a = int.Parse(Console.ReadLine());
+    //  if (a == 0) { result = "NO"; break; }
+    //}
+    //Console.WriteLine(result);
+    /********** AND+OR **********/
+    //1・3問目
+    //int[] AB = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+    //int A = AB[0], B = AB[1];
+    //string result = A >= 10 && B >= 10 ? "YES" : "NO";
+    //string result = A >= 10 || B >= 10 ? "YES" : "NO";
+    //Console.WriteLine(result);
+    //2問目
+    //char C = Console.ReadLine()[0];
+    //string result = char.IsUpper(C) ? "YES" : "NO";
+    //Console.WriteLine(result);
+    //4問目
+    //int X = int.Parse(Console.ReadLine());
+    //string result = X < 10 ? "YES" : "NO";
+    //Console.WriteLine(result);
+    //5問目
+    //int[] AB = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+    //int A = AB[0], B = AB[1];
+    //string result = A >= 10 && B < 10 ? "YES" : "NO";
+    //Console.WriteLine(result);
+    //6問目
+    //int[] XYZ = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+    //int X = XYZ[0], Y = XYZ[1], Z = XYZ[2];
+    //string result = "";
+    //if (Z == 10)
+    //  result = "YES";
+    //else
+    //  result = X >= 10 && Y >= 10 ? "YES" : "NO";
+    //Console.WriteLine(result);
+    /********** FizzBuzz **********/
+    //1～2問目
+    //int N = int.Parse(Console.ReadLine());
+    //string result = N % 3 == 0 ? "YES" : "NO";
+    //string result = N % 3 == 0 && N % 5 == 0 ? "YES" : "NO";
+    //Console.WriteLine(result);
+    //3問目
+    //int N = int.Parse(Console.ReadLine());
+    //int E = 0, O = 0;
+    //int[] As = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+    //for (int i = 0; i < N; i++)
+    //{
+    //  if (As[i] % 2 == 0) { E++; }
+    //  else { O++; }
+    //}
+    //Console.WriteLine(E + " " + O);
+    //4問目
+    //int X = int.Parse(Console.ReadLine());
+    //string[] days = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+    //string result = days[(X - 1) % 7];
+    //Console.WriteLine(result);
+    //5問目
+    //int N = int.Parse(Console.ReadLine());
+    //string result;
+    //if (N % 3 == 0 && N % 5 == 0) { result = "FizzBuzz"; }
+    //else if (N % 3 == 0) { result = "Fizz"; }
+    //else if (N % 5 == 0) { result = "Buzz"; }
+    //else { result = N.ToString(); }
+    //Console.WriteLine(result);
+    /********** 積の最小化 **********/
+    //1問目
+    //int N = int.Parse(Console.ReadLine());
+    //int D = N.ToString().Length;
+    //Console.WriteLine(D);
+    //2問目
+    //int[] NAB = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+    //int N = NAB[0], A = NAB[1], B = NAB[2];
+    //string result = "NO";
+    //if (N + A + B == 0) { result = "YES"; }
+    //else if (N + A - B == 0) { result = "YES"; }
+    //else if (N - A + B == 0) { result = "YES"; }
+    //else if (N - A - B == 0) { result = "YES"; }
+    //Console.WriteLine(result);
+    //3問目
+    //int N = int.Parse(Console.ReadLine());
+    //int[] As = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    //int[] Bs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    //int result = 0;
+    //for (int i = 0; i < N; i++)
+    //{
+    //  if (As[i] == Bs[i]) { result++; }
+    //}
+    //Console.WriteLine(result);
+    //4問目
+    //int N = int.Parse(Console.ReadLine());
+    //int[] As = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    //int result = 0;
+    //for (int i = 0; i < N; i++)
+    //{
+    //  if (As[i] % 2 == 0) { result += As[i]; }
+    //  else { break; }
+    //}
+    //Console.WriteLine(result);
+    //5問目
+    //int[] NK = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    //int N = NK[0], K = NK[1];
+    //int M = 1;
+    //while ((N *= 2) < K)
+    //{
+    //  M++;
+    //}
+    //Console.WriteLine(M);
+    //6～7問目
+    //int[] NKT = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+    //int N = NKT[0], K = NKT[1], T = NKT[2];
+    //string result = K * T % N == 0 ? "YES" : "NO";
+    //string result = K * N <= T ? "YES" : "NO";
+    //Console.WriteLine(result);
+    //8問目
+    //int[] HW = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+    //int H = HW[0], W = HW[1];
+    //string result = H % 2 == 0 && W % 2 == 0 ? "YES" : "NO";
+    //result = H == 0 || W == 0 ? "NO" : result;
+    //Console.WriteLine(result);
+    //9問目
+    int[] AB = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+    int A = AB[0], B = AB[1];
+    int result;
+    if (A >= 0 && B >= 0) { result = A * A; }
+    else if (A < 0 && B >= 0) { result = A * B; }
+    else { result = B * B; }
+    Console.WriteLine(result);
+  }
+}
--- paiza_LevelUp_C/条件分岐メニュー.cs
+++ paiza_LevelUp_C/条件分岐メニュー.cs
@@ -1,9 +1,9 @@
 ﻿//using System;
-//using System.IO;
+//using System.Collections.Generic;
+//using System.ComponentModel;
 //using System.Linq;
-//using System.Reflection;
-//using System.Runtime.InteropServices;
-//using System.Text.RegularExpressions;
+//using System.Net;
+//using System.Reflection.Emit;
 
 //class Program
 //{
@@ -11,206 +11,52 @@
 //  {
 //    //自分の得意な言語で
 //    //Let's チャレンジ！！
-//    //1問目
-//    //Console.WriteLine("paiza");
-//    //2問目
-//    //Console.WriteLine(Console.ReadLine());
-//    //3問目
-//    //string S = Console.ReadLine();
-//    //int index = int.Parse(Console.ReadLine()) - 1;
-//    //Console.WriteLine(S[index]);
-//    //4問目
-//    //string S = Console.ReadLine();
-//    //Console.WriteLine(S == "paiza" ? "YES" : "NO");
+//    /********** a ~ z までを表示 **********/
+//    //1～7問目
+//    //int[] NK = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+//    //int N = NK[0], K = NK[1];
+//    //int N = int.Parse(Console.ReadLine());
+//    //var A = Enumerable.Range(1, N).ToArray();
+//    //char[] A = Enumerable.Range('a', 26).Select(i => (char)i).ToArray();
+//    //var A = Enumerable.Repeat(N, K).ToArray();
+//    //var A = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+//    //Console.WriteLine(string.Join("\n", A));
+//    /********** 数列同士の引き算 **********/
+//    //1～4問目
+//    //var NAB = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+//    //int N = NAB[0], A = NAB[1], B = NAB[2];
+//    //int N = int.Parse(Console.ReadLine());
+//    //var As = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+//    //var Bs = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+//    //Zipメソッドを使用すれば簡潔に書ける!!!
+//    //As = As.Zip(Bs, (a, b) => a - b).ToArray();
+//    //As = As.Select(i => i * 2).ToArray();
+//    //SkipとTakeは使いこなせば武器になる!!!
+//    //Console.WriteLine(As.Skip(A - 1).Take(B - A + 1).Sum());
+//    //Console.WriteLine(As.Sum());
+//    //Console.WriteLine(string.Join("\n", As));
+//    /********** FizzBuzz **********/
+//    //1～4問目
+//    //int N = int.Parse(Console.ReadLine());
+//    //var As = Enumerable.Range(1, 9).Select(i => i * N).ToList();
+//    //var As = Enumerable.Range(1, N).Select(i => Math.Pow(2, i)).ToList();
+//    //var As = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+//    //As.Reverse();
+//    //Console.WriteLine(string.Join("\n", As));
+//    //Console.WriteLine(string.Join(" ", As));
 //    //5問目
-//    //string S=Console.ReadLine();
-//    //Console.WriteLine(S.Length);
-//    //6問目
-//    //string S = Console.ReadLine();
-//    //string T = Console.ReadLine();
-//    //Console.WriteLine(S.IndexOf(T) + 1);
-//    //7問目
+//    //string[] As = Enumerable.Range(1, 100).Select(i => i.ToString()).ToArray();
+//    //for (int i = 3; i <= 100; i += 3) { As[i - 1] = "Fizz"; }
+//    //for (int i = 5; i <= 100; i += 5) { As[i - 1] = "Buzz"; }
+//    //for (int i = 15; i <= 100; i += 15) { As[i - 1] = "FizzBuzz"; }
+//    //Console.WriteLine(string.Join("\n", As));
+//    /********** 数列の中に何個ある? **********/
+//    //1～4問目
 //    //int N = int.Parse(Console.ReadLine());
-//    //string ans = "";
-//    //for (int _index = 0; _index < N; _index++)
-//    //{
-//    //  ans += Console.ReadLine();
-//    //}
-//    //Console.WriteLine(ans);
-//    //8問目
-//    //string S = Console.ReadLine();
-//    //int[] line = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-//    //int start = line[0] - 1, count = line[1] - start;
-//    //Console.WriteLine(S.Substring(start,count));
-//    //9問目
-//    //string S = Console.ReadLine();
-//    //string T = Console.ReadLine();
-//    //int N = int.Parse(Console.ReadLine());
-//    //Console.WriteLine(S.Insert(N, T));
-//    //10問目
-//    //string S = Console.ReadLine();
-//    //string[] sLine = Console.ReadLine().Split();
-//    //int index = int.Parse(sLine[0]) - 1;
-//    //string c = sLine[1];
-//    //S = S.Substring(0, index) + c + S.Substring(index + 1);
-//    //Console.WriteLine(S);
-//    //11問目
-//    //int N = int.Parse(Console.ReadLine());
-//    //Console.WriteLine(N - 813);
-//    //12問目
-//    //int X = int.Parse(Console.ReadLine());
-//    //int Y = int.Parse(Console.ReadLine());
-//    //int n = int.Parse(Console.ReadLine());
-//    //Console.WriteLine((X + Y).ToString()[n-1]); 
-//    //13～14問目
-//    //string S = Console.ReadLine();
-//    //Console.WriteLine(S.ToLower());
-//    //Console.WriteLine(S.ToUpper());
-//    //15問目
-//    //string S = Console.ReadLine();
-//    //string ans = "";
-//    //for (int _index = 0; _index < S.Length; _index++)
-//    //{
-//    //  if (char.IsUpper(S[_index]))
-//    //  {
-//    //    ans += S[_index].ToString().ToLower();
-//    //  }
-//    //  else
-//    //  {
-//    //    ans += S[_index].ToString().ToUpper();
-//    //  }
-//    //}
-//    //Console.WriteLine(ans);
-//    //16問目
-//    //string S = Console.ReadLine();
-//    //string T = Console.ReadLine();
-//    //if (S.Contains(T)) { Console.WriteLine("YES"); }
-//    //else { Console.WriteLine("NO"); }
-//    //17問目
-//    //string S = Console.ReadLine();
-//    //stringを初期化できる!!!)
-//    //S = new string(S.Reverse().ToArray());
-//    //Console.WriteLine(S);
-//    //18問目
-//    //string S = Console.ReadLine();
-//    //string T = new string(S.Reverse().ToArray());
-//    //if (S == T) { Console.WriteLine("YES"); }
-//    //else { Console.WriteLine("NO"); }
-//    //19～21問目
-//    //string[] fLine = Console.ReadLine().Split(',');
-//    //string[] fLine = Console.ReadLine().Split(new char[] { '/', ':' });
-//    //string[] fLine = Console.ReadLine().Split(new char[] { '/', ':', ' ' });
-//    //Console.WriteLine(string.Join("\n", fLine));
-//    //22問目
-//    //string S = Console.ReadLine();
-//    //bool beNum = true;
-//    //foreach (var c in S)
-//    //{
-//    //  if(!int.TryParse(c.ToString(),out int tmp))
-//    //  {
-//    //    beNum = false;
-//    //    break;
-//    //  }
-//    //}
-//    //if (beNum) { Console.WriteLine("YES"); }
-//    //else { Console.WriteLine("NO"); }
-//    //23問目
-//    //char[] fLine = Console.ReadLine().Distinct().ToArray();
-//    //Console.WriteLine(string.Join("",fLine));
-//    //24問目
-//    //int N = int.Parse(Console.ReadLine());
-//    //int Q = int.Parse(Console.ReadLine());
-//    //char[] ans = new char[N];
-//    //for (int index = 0; index < Q; index++)
-//    //{
-//    //  var line = Console.ReadLine().Split();
-//    //  int index = int.Parse(line[0]) - 1;
-//    //  ans[index] = line[1][0];
-//    //}
-//    //char c = Console.ReadLine()[0];
-//    //for (int index = 0; index < N; index++)
-//    //{
-//    //  ans[index] = ans[index] == '\0' ? c : ans[index];
-//    //}
-//    //Console.WriteLine(string.Join("", ans));
-//    //25問目(正規表現を初めて知る!!!)
-//    //string S = Console.ReadLine();
-//    //if (S.Contains('.'))
-//    //{
-//    //  int index = S.IndexOf('.');
-//    //  S = S.Substring(0, index + 1) + S.Substring(index + 1).Replace(".", "");
-//    //  S = Regex.Replace(S, "$0+", "");
-//    //  while (S.Last() == '0')
-//    //  {
-//    //    S = S.Trim('0');
-//    //    if (S.Last() == '.')
-//    //    {
-//    //      S = S.Replace(".", "");
-//    //      break;
-//    //    }
-//    //  }
-//    //}
-//    //S = Regex.Replace(S, "^0+", "");
-//    //if (S[0] == '.')
-//    //{
-//    //  S = S.Insert(0, "0");
-//    //}
-//    //Console.WriteLine(S);
-//    //26～27問目
-//    //string S = Console.ReadLine();
-//    //S += ".";
-//    //int ans = 0;
-//    //char operand = '+';
-//    //string strNum = "";
-
-//    //foreach (var c in S)
-//    //{
-//    //  if (int.TryParse(c.ToString(), out int tmp))
-//    //  {
-//    //    strNum += tmp;
-//    //  }
-//    //  else
-//    //  {
-//    //    ans = CalOperand(operand, ans, int.Parse(strNum));
-//    //    strNum = "";
-//    //    operand = c;
-//    //  }
-//    //}
-//    //Console.WriteLine(ans);
-//    //28～29問目
-//    //string S = Console.ReadLine();
-//    //string T = Console.ReadLine();
-//    //string ans = "";
-//    //int carry = 0;
-//    //for (int i = S.Length - 1; i >= 0; i--)
-//    //{
-//    //  int product = int.Parse(S[i].ToString()) + int.Parse(T[i].ToString()) + carry;
-//    //  carry = product / 10;
-//    //  ans += product % 10;
-//    //}
-//    //ans += carry == 1 ? "1" : "";
-//    //ans = new string(ans.Reverse().ToArray());
-//    //Console.WriteLine(ans);
-//    //30問目
-//    //string S = Console.ReadLine();
-//    //int T = int.Parse(Console.ReadLine());
-//    //if (T == 0) { Console.WriteLine(0); return; }
-//    //string ans = "";
-//    //int carry = 0;
-//    //for (int i = S.Length - 1; i >= 0; i--)
-//    //{
-//    //  int product = int.Parse(S[i].ToString()) * T + carry;
-//    //  carry = product / 10;
-//    //  ans += product % 10;
-//    //}
-//    //ans += carry == 0 ? "" : carry.ToString();
-//    //ans = new string(ans.Reverse().ToArray());
-//    //Console.WriteLine(ans);
+//    //var As = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+//    //Console.WriteLine(As.Max());
+//    //Console.WriteLine(As.Min());
+//    //Console.WriteLine(As.IndexOf(1) + 1);
+//    //Console.WriteLine(As.Count(i => i == 1));
 //  }
-
-//  //public static int CalOperand(char operand, int ans, int num)
-//  //{
-//  //if文簡潔化できる!!!
-//  //  return operand == '+' ? ans + num : ans - num;
-//  //}
 //}
