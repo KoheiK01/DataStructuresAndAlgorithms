6def2f658af66b29cf942411ec64f0fc116feb9d
Kohei <128622577+KoheiK01@users.noreply.github.com>
2024-10-28 16:08:38 +0900

    Finish paiza_LevelUp_C

--- paiza_LevelUp_C/条件式メニュー.cs
+++ paiza_LevelUp_C/条件式メニュー.cs
@@ -1,36 +1,36 @@
-﻿class Program
-{
-  static void Main()
-  {
-    //自分の得意な言語で
-    //Let's チャレンジ！！
-    /********** 文字列の比較 **********/
-    //1～4問目
-    //string[] words = Console.ReadLine().Split();
-    //int A = int.Parse(words[0]), B = int.Parse(words[1]);
-    //Console.WriteLine(A == B);
-    //Console.WriteLine(A != B);
-    //Console.WriteLine(A < B);
-    //Console.WriteLine(A <= B);
-    //5問目
-    //string[] words = Console.ReadLine().Split();
-    //int A = int.Parse(words[0]), B = int.Parse(words[1]), C = int.Parse(words[2]), D = int.Parse(words[3]);
-    //Console.WriteLine(A + B == C * D);
-    //6問目
-    //string[] words = Console.ReadLine().Split(' ');
-    //Console.WriteLine(words[0] == words[1]);
-    /********** 論理演算子の組み合わせ 3 **********/
-    //1～2・4～7問目
-    //string[] words = Console.ReadLine().Split();
-    //int A = int.Parse(words[0]), B = int.Parse(words[1]), C = int.Parse(words[2]), D = int.Parse(words[3]);
-    //Console.WriteLine(A == B && C == D);
-    //Console.WriteLine(A == B || C == D);
-    //Console.WriteLine(!(A == B && C == D));
-    //Console.WriteLine(A == B && B == C && C == D);
-    //Console.WriteLine((A == B || C == D) && A == C);
-    //3問目
-    //string[] words = Console.ReadLine().Split();
-    //int A = int.Parse(words[0]), B = int.Parse(words[1]);
-    //Console.WriteLine(A != B);
-  }
-}
+﻿//class Program
+//{
+//  static void Main()
+//  {
+//    //自分の得意な言語で
+//    //Let's チャレンジ！！
+//    /********** 文字列の比較 **********/
+//    //1～4問目
+//    //string[] words = Console.ReadLine().Split();
+//    //int A = int.Parse(words[0]), B = int.Parse(words[1]);
+//    //Console.WriteLine(A == B);
+//    //Console.WriteLine(A != B);
+//    //Console.WriteLine(A < B);
+//    //Console.WriteLine(A <= B);
+//    //5問目
+//    //string[] words = Console.ReadLine().Split();
+//    //int A = int.Parse(words[0]), B = int.Parse(words[1]), C = int.Parse(words[2]), D = int.Parse(words[3]);
+//    //Console.WriteLine(A + B == C * D);
+//    //6問目
+//    //string[] words = Console.ReadLine().Split(' ');
+//    //Console.WriteLine(words[0] == words[1]);
+//    /********** 論理演算子の組み合わせ 3 **********/
+//    //1～2・4～7問目
+//    //string[] words = Console.ReadLine().Split();
+//    //int A = int.Parse(words[0]), B = int.Parse(words[1]), C = int.Parse(words[2]), D = int.Parse(words[3]);
+//    //Console.WriteLine(A == B && C == D);
+//    //Console.WriteLine(A == B || C == D);
+//    //Console.WriteLine(!(A == B && C == D));
+//    //Console.WriteLine(A == B && B == C && C == D);
+//    //Console.WriteLine((A == B || C == D) && A == C);
+//    //3問目
+//    //string[] words = Console.ReadLine().Split();
+//    //int A = int.Parse(words[0]), B = int.Parse(words[1]);
+//    //Console.WriteLine(A != B);
+//  }
+//}
