9be4d56e07fb78ccab296afc53b7d29b72c6985f
Kohei <128622577+KoheiK01@users.noreply.github.com>
2024-10-28 16:09:57 +0900

    Finish paiza_LevelUp_D

--- paiza_LevelUp_D/Dランク・スキルチェック過去問題セット.cs
+++ paiza_LevelUp_D/Dランク・スキルチェック過去問題セット.cs
@@ -1,21 +1,21 @@
-﻿using System;
-using System.Runtime.InteropServices;
+﻿//using System;
+//using System.Runtime.InteropServices;
 
-class Program
-{
-  static void Main()
-  {
-    // 自分の得意な言語で
-    // Let's チャレンジ！！
-    //1問目
-    //string _s = Console.ReadLine();
-    //string _t = Console.ReadLine();
-    //Console.WriteLine(_s + "@" + _t);
-    //2問目
-    int N=int.Parse(Console.ReadLine());
-    for (int i=0; i<N; i++)
-    {
-      Console.Write("*");
-    }
-  }
-}
+//class Program
+//{
+//  static void Main()
+//  {
+//    // 自分の得意な言語で
+//    // Let's チャレンジ！！
+//    //1問目
+//    //string _s = Console.ReadLine();
+//    //string _t = Console.ReadLine();
+//    //Console.WriteLine(_s + "@" + _t);
+//    //2問目
+//    int N=int.Parse(Console.ReadLine());
+//    for (int i=0; i<N; i++)
+//    {
+//      Console.Write("*");
+//    }
+//  }
+//}
